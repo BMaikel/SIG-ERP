22281af6baad9abcf0d6fe9e5aa9b72ae2246c1e
Michael Alexis <20210824@lamolina.edu.pe>
2023-11-06 15:48:52 -0500

    login template

--- app/app.py
+++ app/app.py
@@ -8,7 +8,7 @@ db = Database()
 #LOGIN ------------------------------------------------------------
 @app.route("/login", methods = ["GET", "POST"])
 def login():
-    return render_template("index.html")
+    return render_template("login.html")
 
 if __name__ == "__main__":
-    app.run(debug=True) 
+    app.run(debug=True)
---
+++ app/prueba.py
@@ -0,0 +1,2 @@
+import os 
+print(os.getenv('ASSETS_ROOT', '/static/assets'))
