6df9f7ebc6f31609e507d4928feb20ca0742a04d
Michael Alexis <20210824@lamolina.edu.pe>
2023-11-07 18:34:51 -0500

    login terminado

--- app/app.py
+++ app/app.py
@@ -1,55 +1,54 @@
 from bd_cnn import Database
-from user_model import User
 
 from flask import Flask, render_template, request, redirect, url_for, session
-from flask_login import LoginManager, login_required, login_user, logout_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "MySecretKey"
 
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-
 db = Database()
 
 #LOGIN ------------------------------------------------------------
-@login_manager.user_loader
-def load_user(user_id):
-    return db.get_user(user_id) 
-
 @app.route("/login", methods = ["GET", "POST"])
 def login():
     if request.method == "POST":
         username = request.form["username"]
         pwd = request.form["pass"]
 
-        resp = db.buscar_usuario(username, pwd)
+        user = db.buscar_usuario(username, pwd)
 
-        if resp != None:
-            user = User(resp[0],resp[1],resp[2],resp[1])
-            login_user(user)
-            return redirect(url_for("home"))
+        if user:
+
+            session["id"]           = user[0]
+            session["nombres"]      = user[1]
+            session["apellidos"]    = user[2]
+            session['usuario']      = user[4]
+            session["pass"]         = user[5]
+
+            return redirect(url_for('index'))
         
         else:
-            return render_template("login.html", error = "Usuario o contraseña no válido :c")
-        
+            return render_template("login.html", error = "Usuario o contraseña no válido :c")  
     else:
         return render_template("login.html")
     
+
 @app.route('/logout')
-@login_required
 def logout():
-    logout_user()
-    return redirect(url_for('login'))
+    session.pop('id', None)
+    session.pop('nombres', None)
+    session.pop('apellidos', None)
+    session.pop('usuario', None)
+    session.pop('pass', None)
+    return redirect(url_for('index'))
 
 #DASHBOARD P1 -----------------------------------------------------
-@app.route('/home')
-@login_required
-def home():
-    if "username" in session:
-        return "Página permitida solo con Inicio de Sesión :3"
-    else:
-        return "Inicia sesión perro"
+@app.route('/')
+def index():
+    if 'usuario' in session:
+        return render_template("home_c.html")
+    
+    return redirect(url_for('login'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- app/test.py
+++
@@ -1,6 +0,0 @@
-from bd_cnn import Database
-
-db = Database()
-
-a = db.buscar_usuario("admin","admin")
-db.close
